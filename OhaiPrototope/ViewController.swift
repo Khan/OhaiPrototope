31c260901d984bbcf00ae4f531474ca5511e1681
May-Li Khoe <mayli@khanacademy.org>
2015-01-27 17:28:34 -0800

    Added two-finger double tap to switch scenes

--- OhaiPrototope/ViewController.swift
+++ OhaiPrototope/ViewController.swift
@@ -12,26 +12,51 @@ import Prototope
 class ViewController: UIViewController {
 
     var mainScene: AnyObject! // likely some form of 'scene'
+    
+    var sceneNumber = 0
+    
+    var sceneArray: [() -> AnyObject] = [ { TouchEvents() }, { ShadowLightsource() }, { TouchAnimators() }]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // set the view controller's view as the Prototope Root View
         Layer.setRoot(fromView: view)
+        
+        Layer.root.gestures.append(TapGesture (numberOfTouchesRequired: 2, numberOfTapsRequired: 2) { _ in
+            
+            self.nextState()
+            
+            })
 
         // initialize the Touch Events demo
-//        mainScene = TouchEvents()
+        mainScene = TouchEvents()
 
         // possibly initialize other ones instead
         // mainScene = ShadowLightsource()
-        mainScene = TouchAnimators()
+        //mainScene = TouchAnimators()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    func nextState() {
+
+        if (sceneNumber >= sceneArray.count - 1) {
+            sceneNumber = 0
+        } else {
+            sceneNumber++
+        }
+        
+        Layer.root.removeAllSublayers()
+        mainScene = sceneArray[sceneNumber]()
+        
+
+    }
 
 }
 
