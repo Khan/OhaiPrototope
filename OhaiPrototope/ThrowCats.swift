0f82177b4efa676d7785cc854489ffeafbb66ab3
Saniul Ahmed <saniul.ahmed@gmail.com>
2015-02-06 02:04:46 -0800

    decay sketch

--- OhaiPrototope/ThrowCats.swift
+++ OhaiPrototope/ThrowCats.swift
@@ -27,20 +27,28 @@ class ThrowCats {
         catLayer.x = 400
         catLayer.y = 512
 
-        //insert code to bring cat back here. 
+        //insert code to bring cat back here.
+        
+        let catUpdateHandler: () -> Void = {
+            if (self.catLayer.position.x > 0 && self.catLayer.position.x < self.catLayer.parent!.width &&
+                self.catLayer.position.y > 0 && self.catLayer.position.y < self.catLayer.parent!.height) {
+                    return;
+            }
+            self.catLayer.animators.position.stop()
+            self.catLayer.position = Point(x: 400, y: 512)
+        }
         
         catLayer.gestures.append(PanGesture(cancelsTouchesInLayer: false) { phase, sequence in
             switch phase {
-            case .Began: break // Do nothing
+            case .Began:
+                self.catLayer.animators.position.stop()
             case .Changed:
                 self.catLayer.position += sequence.currentSample.locationInLayer(self.catLayer.parent!) - sequence.previousSample!.locationInLayer(self.catLayer.parent!)
             case .Ended, .Cancelled:
                 var catVelocity = sequence.currentVelocityInLayer(self.catLayer.parent!)
-                var catGoal =  Point(x:(self.catLayer.position.x + catVelocity.x), y: (self.catLayer.position.y + catVelocity.y))
-                self.catLayer.animators.position.target = catGoal
-                self.catLayer.animators.position.velocity = sequence.currentVelocityInLayer(self.catLayer.parent!)
-                self.catLayer.animators.position.springBounciness = 0 //no springiness for this cat
-                
+                let animation = DecayAnimation(velocity: catVelocity)
+                self.catLayer.animators.position.animationKind = animation
+                self.catLayer.animators.position.applyHandler = catUpdateHandler
             }
         })
         
