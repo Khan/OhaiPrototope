057de9b8bd9492e63d6f6f16c54503f5ade97eef
Marcos Ojeda <subliminal@gmail.com>
2015-02-05 22:07:12 -0800

    Handle j/k keypresses to advance/rewind scenes respectively
    adds handler to viewcontroller and refactors nextScene to increment
    or decrement scenes (renamed to `jumpScene`)

--- OhaiPrototope/ViewController.swift
+++ OhaiPrototope/ViewController.swift
@@ -12,9 +12,9 @@ import Prototope
 class ViewController: UIViewController {
 
     var mainScene: AnyObject! // likely some form of 'scene'
-    
+
     var sceneNumber = 0
-    
+
     // add any new scenes to this array in order to allow cycling through with double-double taps
     var sceneArray: [() -> AnyObject] = [ { TouchEvents() }, { ShadowLightsource() }, { TouchAnimators() }, { TouchUnicorns() }, { RotationGesturePlaypen() }, { ThrowCats() }]
 
@@ -23,11 +23,11 @@ class ViewController: UIViewController {
 
         // set the view controller's view as the Prototope Root View
         Layer.setRoot(fromView: view)
-        
+
         Layer.root.gestures.append(TapGesture (numberOfTouchesRequired: 2, numberOfTapsRequired: 2) { _ in
-            
-            self.nextState()
-            
+
+            self.jumpScene(1)
+
             })
 
         // initialize the Touch Events demo
@@ -36,27 +36,47 @@ class ViewController: UIViewController {
         // possibly initialize other ones instead
         // mainScene = ShadowLightsource()
         //mainScene = TouchAnimators()
-        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    func nextState() {
+    // advance or go back to scene in sceneArray by relative
+    // amount, not index
+    func jumpScene(amount: Int) {
+        sceneNumber += amount + sceneArray.count;
+        sceneNumber %= sceneArray.count;
 
-        if (sceneNumber >= sceneArray.count - 1) {
-            sceneNumber = 0
-        } else {
-            sceneNumber++
-        }
-        
         Layer.root.removeAllSublayers()
         mainScene = sceneArray[sceneNumber]()
-        
+    }
 
+
+    func handleKeyCommand(command: UIKeyCommand!) {
+        switch command.input {
+        case "j":
+            jumpScene(1)
+        case "k":
+            jumpScene(-1)
+        default:
+            return
+        }
+    }
+
+    // needed to let vc handle keypresses
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+
+    override var keyCommands: [AnyObject]? {
+        get {
+            let prev = UIKeyCommand(input: "k", modifierFlags: nil, action: "handleKeyCommand:")
+            let next = UIKeyCommand(input: "j", modifierFlags: nil, action: "handleKeyCommand:")
+            return [prev, next]
+        }
     }
 
 }
