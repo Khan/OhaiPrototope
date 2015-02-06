3e62d091cd45bc5a78769ec35e51756ac4902ddd
Andy Matuschak <andy@khanacademy.org>
2015-02-06 11:24:39 -0800

    Added navigation UI

--- OhaiPrototope/AppDelegate.swift
+++ OhaiPrototope/AppDelegate.swift
@@ -10,37 +10,40 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
+    var window: UIWindow!
+	var navigationController: UINavigationController!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+		window = UIWindow(frame: UIScreen.mainScreen().bounds)
+		window.makeKeyAndVisible()
+
+		let sceneListingViewController = SceneListingViewController(
+			scenes: Scene.sceneIndex,
+			sceneActivationHandler: { [unowned self] in self.activateScene($0) }
+		)
+		sceneListingViewController.title = "Prototypes"
+		navigationController = UINavigationController(rootViewController: sceneListingViewController)
+		navigationController.interactivePopGestureRecognizer.enabled = false
+		window.rootViewController = navigationController
+
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
+	private func activateScene(scene: Scene) {
+		let sceneViewController = SceneViewController(scene: scene)
+		sceneViewController.backActionHandler = {
+			self.navigationController.setNavigationBarHidden(false, animated: true)
+			self.navigationController.popViewControllerAnimated(true)
+		}
+		navigationController.setNavigationBarHidden(true, animated: true)
+		navigationController.pushViewController(sceneViewController, animated: true)
 
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+		if !NSUserDefaults.standardUserDefaults().boolForKey("hasSeenNavigationWarning") {
+			UIAlertView(title: "Navigation Tutorial", message: "Type Escape or press the Volume Up button to navigate back.\n\nYou won't see this message again.", delegate: nil, cancelButtonTitle: "OK").show()
+			NSUserDefaults.standardUserDefaults().setBool(true, forKey:"hasSeenNavigationWarning")
+		}
+	}
 
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+	
 }
 
---
+++ OhaiPrototope/SceneIndex.swift
@@ -0,0 +1,23 @@
+//
+//  SceneIndex.swift
+//  OhaiPrototope
+//
+//  Created by Andy Matuschak on 2/6/15.
+//  Copyright (c) 2015 Marcos Ojeda. All rights reserved.
+//
+
+import Foundation
+
+struct Scene {
+	let name: String
+	let constructor: () -> AnyObject // what *are* types, anyway?
+
+	static let sceneIndex: [Scene] = [
+		Scene(name: "Touch animators", constructor: { TouchAnimators() }),
+		Scene(name: "Shadow light source", constructor: { ShadowLightsource() }),
+		Scene(name: "Touch events", constructor: { TouchEvents() }),
+		Scene(name: "Touch unicorns", constructor: { TouchUnicorns() }),
+		Scene(name: "Rotation gesture playpen", constructor: { RotationGesturePlaypen() }),
+		Scene(name: "Throw cats", constructor: { ThrowCats() }),
+	]
+}
