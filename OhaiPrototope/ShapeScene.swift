fafd05ddc03f47ae36624eb2ee9b1950c604066f
Jason Brennan <i.jasonbrennan@gmail.com>
2015-03-31 10:22:02 -0400

    Mostly functional "snake" demo.
    There is a coordinate space problem, though.

--- OhaiPrototope/ShapeScene.swift
+++ OhaiPrototope/ShapeScene.swift
@@ -15,6 +15,7 @@ class ShapeScene {
 	let ovalLayer: ShapeLayer
 	let lineLayer: ShapeLayer
 	let pathLayer: ShapeLayer
+	let chainLayer: ShapeLayer
 	
 	init() {
 		Layer.root.backgroundColor = Color(hex: 0xFFF5D9)
@@ -38,10 +39,52 @@ class ShapeScene {
 		self.pathLayer.strokeColor = Color.red
 		self.pathLayer.fillColor = nil
 		self.pathLayer.strokeWidth = 10
-		self.pathLayer.capStyle = .Round
+		self.pathLayer.lineCapStyle = .Round
 		self.pathLayer.lineJoinStyle = .Round
 		
 		self.pathLayer.moveToCenterOfParentLayer()
+		
+		var points = 20
+		var length = 25
+		let chainPath = Path()
+		
+		var startPoint = Point(x: 0, y: 100)
+		for var i = 0; i < points; i++ {
+			chainPath.addPoint(startPoint + Point(x: Double(i * length), y: 0.0))
+		}
+		
+		
+		
+		self.chainLayer = ShapeLayer(path: chainPath)
+		self.chainLayer.strokeWidth = 20
+		self.chainLayer.lineCapStyle = .Round
+		
+		self.chainLayer.moveToCenterOfParentLayer()
+		self.chainLayer.fillColor = nil
+		self.chainLayer.lineJoinStyle = .Round
+
+		
+		Layer.root.touchMovedHandler = { (touchSequence: TouchSequence) in
+			
+			let touchPoint = touchSequence.currentSample.globalLocation
+			let firstSegment = Segment(point: touchPoint)
+			chainPath.replaceSegmentAtIndex(0, withSegment: firstSegment)
+			
+			for var i = 0; i < points - 1; i++ {
+				var segment = chainPath.segments[i]
+				var nextSegment = chainPath.segments[i + 1]
+				var vector = segment.point - nextSegment.point
+				var vectorLength = vector.length
+				var scale = Double(length) / vectorLength
+				vector.x = vector.x * scale
+				vector.y = vector.y * scale
+				
+				var newNextSegment = Segment(point: segment.point - vector)
+				chainPath.replaceSegmentAtIndex(i + 1, withSegment: newNextSegment)
+				
+			}
+			self.chainLayer.updatePath(chainPath)
+		}
 		
 		self.rectLayer.backgroundColor = Color.lightGray
 		self.roundRectLayer.backgroundColor = Color.lightGray
