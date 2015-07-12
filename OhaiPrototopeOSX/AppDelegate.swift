8bb4d8397f77b47ae2a20f2e13fd0b5c97d3644d
Jason Brennan <i.jasonbrennan@gmail.com>
2015-07-12 18:03:42 -0400

    Add OhaiPrototopeOSX framework target.

---
+++ OhaiPrototopeOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OhaiPrototopeOSX
+//
+//  Created by Jason Brennan on 2015-07-12.
+//  Copyright (c) 2015 Prototope Research Facility. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ OhaiPrototopeOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OhaiPrototopeOSX
+//
+//  Created by Jason Brennan on 2015-07-12.
+//  Copyright (c) 2015 Prototope Research Facility. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
