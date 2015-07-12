7cdc0dd5c7445ee94aecacc81afe935d12718a9f
Jason Brennan <i.jasonbrennan@gmail.com>
2015-07-12 18:45:52 -0400

    Testing out Speech.

--- OhaiPrototopeOSX/ViewController.swift
+++ OhaiPrototopeOSX/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import PrototopeOSX
 
 class ViewController: NSViewController {
 
@@ -14,6 +15,7 @@ class ViewController: NSViewController {
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
+		Speech.say("oh hi", rate: 200)
 	}
 
 	override var representedObject: AnyObject? {
