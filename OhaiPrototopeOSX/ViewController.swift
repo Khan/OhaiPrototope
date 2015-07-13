437a9e7713e00307778850e0b74077d2c37607e3
Jason Brennan <i.jasonbrennan@gmail.com>
2015-07-12 21:49:13 -0400

    Layer on OS X!

--- OhaiPrototopeOSX/ViewController.swift
+++ OhaiPrototopeOSX/ViewController.swift
@@ -10,12 +10,23 @@ import Cocoa
 import PrototopeOSX
 
 class ViewController: NSViewController {
+	
+	var environment: Environment!
+	var layer: Layer!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
-		Speech.say("oh hi", rate: 200)
+//		Speech.say("oh hi", rate: 200)
+		self.view.wantsLayer = true
+		self.environment = Environment.defaultEnvironmentWithRootView(self.view)
+		Environment.currentEnvironment = self.environment
+		
+		self.layer = Layer(parent: nil, name: "My layer", viewClass: nil)
+		self.layer.size = Size(width: 200, height: 250)
+		self.layer.backgroundColor = Color.purple
+		self.layer.border = Border(color: Color.orange, width: 5)
 	}
 
 	override var representedObject: AnyObject? {
